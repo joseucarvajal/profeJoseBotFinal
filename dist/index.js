f1fca2230d9e757df4aade8b53f6b5fa0d8ab0d6
joseucarvajal <joseucarvajal@gmail.com>
2018-12-18 12:43:15 -0500

    index test

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,22 @@
 "use strict";
-/**
- * Infraestructura
- */
 require('./initDatabase');
 require('./initBot');
-/**
- * Módulos
- */
 require('./modulos/index');
+// using the http module
+var http = require('http'), 
+// look for PORT environment variable, 
+// else look for CLI argument,
+// else use hard coded value for port 8080
+port = process.env.PORT || process.argv[2] || 8080;
+// create a simple server
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.write('hello heroku!', 'utf-8');
+    res.end();
+});
+// listen on the port
+server.listen(port, function () {
+    console.log('app up on port: ' + port);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
-/**
- * Infraestructura
- */
+
 require('./initDatabase');
 require('./initBot');
 
-/**
- * Módulos
- */
-
 require('./modulos/index');
+
+
+// using the http module
+let http = require('http'),
+ 
+// look for PORT environment variable, 
+// else look for CLI argument,
+// else use hard coded value for port 8080
+port = process.env.PORT || process.argv[2] || 8080;
+ 
+// create a simple server
+let server = http.createServer(function (req:any, res:any) {
+ 
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello heroku!', 'utf-8');
+        res.end();
+ 
+    });
+ 
+// listen on the port
+server.listen(port, function () {
+ 
+    console.log('app up on port: ' + port);
+ 
+});
 
