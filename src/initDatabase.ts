c2ae5b19dad4e7d6991edd259d65e4c9112e2931
josecarvajalrego <jose.carvajal@regoconsulting.com>
2019-02-01 21:57:47 -0500

    api change v2.2

--- src/initDatabase.ts
+++ src/initDatabase.ts
@@ -2,7 +2,7 @@ const firebase = require('firebase');
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCU9AvEF_dbdjMpbKNc9BJbIIFTAa_vW0s",
+    apiKey: "AIzaSyAr8MHVp9LKeRGlKUirYao4JQrRGMIjdEQ",
     authDomain: "profejosebot-94c5e.firebaseapp.com",
     databaseURL: "https://profejosebot-94c5e.firebaseio.com",
     projectId: "profejosebot-94c5e",
